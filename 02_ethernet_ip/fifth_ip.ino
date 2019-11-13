3ac4f3b0ac280e83e4220d0ecd05786299d57f50
Jun Jae Nam <manisjun@y-y.hs.kr>
2019-11-13 10:53:32 +0900

    주석 제거

--- 02_ethernet_ip/fifth_ip.ino
+++ 02_ethernet_ip/fifth_ip.ino
@@ -22,7 +22,7 @@ void setup() {
   }
   
   // print your local IP address:
-  IPAddress myIP = Ethernet.localIP();  //ip 주소 설정
+  IPAddress myIP = Ethernet.localIP();
   Serial.print("My IP address: ");
   Serial.println(myIP);
   ip_display(myIP);
